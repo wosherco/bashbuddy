b78a0faf0b7c27b8ab5848344676617f1fa1aa3f
polvallverdu <pvallverdualcaide@gmail.com>
2025-03-05 20:31:30 +0100

    fix: fixed builds not going through

--- packages/posthog/src/index.ts
+++ packages/posthog/src/index.ts
@@ -2,14 +2,15 @@ import { PostHog } from "posthog-node";
 
 import { env } from "../env";
 
-export const posthog = new PostHog(env.PUBLIC_POSTHOG_API_KEY, {
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+export const posthog = new PostHog(env.PUBLIC_POSTHOG_API_KEY ?? "phc_dummy", {
   host: env.PUBLIC_POSTHOG_ENDPOINT,
 });
 
-process.on("SIGTERM", async () => {
-  await posthog.shutdown();
+process.on("SIGTERM", () => {
+  void posthog.shutdown();
 });
 
-process.on("exit", async () => {
-  await posthog.shutdown();
+process.on("exit", () => {
+  void posthog.shutdown();
 });
