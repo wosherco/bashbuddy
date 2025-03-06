e89744b566bbfea4229dabdda9c255a9de510349
polvallverdu <pvallverdualcaide@gmail.com>
2025-03-06 19:48:37 +0100

    fix: temp disabled tests gh action

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,13 +1,3 @@
 import { defineWorkspace } from "vitest/config";
 
-export default defineWorkspace([
-  // "packages/*",
-  {
-    test: {
-      name: "rrule",
-      root: "./packages/rrule",
-      environment: "node",
-      globalSetup: "../../vitest.global-setup.ts",
-    },
-  },
-]);
+export default defineWorkspace([]);
